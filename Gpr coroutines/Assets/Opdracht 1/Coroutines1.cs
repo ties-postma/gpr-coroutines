693001bf91e34875a969cdd6dfe37cf7db5c3e32
ties-postma <28114@ma-web.nl>
2021-02-15 03:16:00 +0100

    opdracht 1

---
+++ Gpr coroutines/Assets/Opdracht 1/Coroutines1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coroutines1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Fade");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Fade ()
+    {
+        Debug.Log("Coroutine update");
+        yield return new WaitForSeconds(.5f);
+        Debug.Log("Coroutine update2");
+        yield return new WaitForSeconds(.5f);
+        Debug.Log("Coroutine update3");
+        yield return new WaitForSeconds(.5f);
+    }
+}
